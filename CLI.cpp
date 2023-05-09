07e16ada0f79f985baa6494b08accc22666b91b2
Arnav Chhokra <arnavchhokra@gmail.com>
2023-05-09 21:39:42 +0530

    secondary commit
    Implemented classes and try catch blocks

--- CLI.cpp
+++ CLI.cpp
@@ -4,110 +4,100 @@
 
 using namespace std;
 
-void displayHelp()
-{
-    cout << "Commands:\n";
-    cout<< "dir [path] - List the files and directories in the specified path\n";
-    cout<< "md [path] - Create a new directory with the specified path\n";
-    cout<< "rd [path] - Delete the directory with the specified path\n";
-    cout<< "exit - Exit the program\n";
-}
+class Directory {
+public:
+    static void listDirectory(const string& path) {
+        HANDLE hFind;
+        WIN32_FIND_DATA data;
+        string searchPath = path + "\\*.*";
+        hFind = FindFirstFile(searchPath.c_str(), &data);
+        if (hFind != INVALID_HANDLE_VALUE) {
+            do {
+                if ((data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) != 0) {
+                    cout << "<DIR> ";
+                }
+                else {
+                    cout << "      ";
+                }
+                cout << data.cFileName << endl;
+            } while (FindNextFile(hFind, &data));
+            FindClose(hFind);
+        }
+        else {
+            throw runtime_error("Error: Unable to list directory");
+        }
+    }
 
-void listDirectory(string path)
-{
-    HANDLE hFind;
-    WIN32_FIND_DATA data;
-    string searchPath = path + "\\*.*";
+    static bool createDirectory(const string& path) {
+        if (CreateDirectory(path.c_str(), NULL)) {
+            cout << "Directory created: " << path << endl;
+            return true;
+        }
+        else {
+            throw runtime_error("Error: Unable to create directory");
+        }
+    }
 
-    hFind = FindFirstFile(searchPath.c_str(), &data);
+    static bool deleteDirectory(const string& path) {
+        if (RemoveDirectory(path.c_str())) {
+            cout << "Directory deleted: " << path << endl;
+            return true;
+        }
+        else {
+            throw runtime_error("Error: Unable to delete directory");
+        }
+    }
+};
 
-    if (hFind != INVALID_HANDLE_VALUE)
-    {
-        do
-        {
-            if ((data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) != 0)
-            {
-                cout << "<DIR> ";
+class CommandLine {
+public:
+    static void displayHelp() {
+        cout << "Commands:\n";
+        cout << "dir [path] - List the files in the specified path\n";
+        cout << "md [path] - Create a new directory in the specified path\n";
+        cout << "rd [path] - Delete the directory of the specified path\n";
+        cout << "exit - Exit the program\n";
+    }
+
+    static void run() {
+        string command;
+        string path;
+
+        while (true) {
+            cout << "> ";
+            cin >> command;
+
+            try {
+                if (command == "dir") {
+                    cin >> path;
+                    Directory::listDirectory(path);
+                }
+                else if (command == "md") {
+                    cin >> path;
+                    Directory::createDirectory(path);
+                }
+                else if (command == "rd") {
+                    cin >> path;
+                    Directory::deleteDirectory(path);
+                }
+                else if (command == "exit") {
+                    break;
+                }
+                else if (command == "help") {
+                    displayHelp();
+                }
+                else {
+                    cout << "Wrong input \n";
+                }
             }
-            else
-            {
-                cout << "      ";
+            catch (const exception& e) {
+                cerr << e.what() << endl;
             }
-            cout << data.cFileName << endl;
-        } while (FindNextFile(hFind, &data));
-        FindClose(hFind);
-    }
-    else
-    {
-        cout << "Error: " << GetLastError() << endl;
-    }
-}
-
-bool createDirectory(string path)
-{
-    if (CreateDirectory(path.c_str(), NULL))
-    {
-        cout << "Directory created: " << path << endl;
-        return true;
-    }
-    else
-    {
-        cout << "Error: " << GetLastError() << endl;
-        return false;
-    }
-}
-
-bool deleteDirectory(string path)
-{
-    if (RemoveDirectory(path.c_str()))
-    {
-        cout << "Directory deleted: " << path << endl;
-        return true;
-    }
-    else
-    {
-        cout << "Error: " << GetLastError() << endl;
-        return false;
-    }
-}
-
-int main()
-{
-    string command;
-    string path;
-
-    while (true)
-    {
-        cout << "> ";
-        cin >> command;
-
-        if (command == "dir")
-        {
-            cin >> path;
-            listDirectory(path);
-        }
-        else if (command == "md")
-        {
-            cin >> path;
-            createDirectory(path);
-        }
-        else if (command == "rd")
-        {
-            cin >> path;
-            deleteDirectory(path);
-        }
-        else if (command == "exit")
-        {
-            break;
-        }
-        else if(command == "help")
-        {
-            displayHelp();
-        }
-        else{
-            cout<<"Wrong input \n";
         }
     }
+};
 
+int main() {
+    CommandLine::run();
     return 0;
 }
